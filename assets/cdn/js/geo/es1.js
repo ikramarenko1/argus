4c6bdbf1329187d942ff1f4641738ccec7904bef
Ilya Kramarenko <il.kramarenko1@gmail.com>
2023-02-07 20:34:42 +0200

    initial

---
+++ assets/cdn/js/geo/es1.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    window.countryList = {
+        es: {
+            name: 'España',
+            code: 'es',
+            price: 49,
+            oldPrice: 98,
+            labelPrice: 'EUR',
+            phoneHelper: 'Por ejemplo, +34 123456789',
+            nameHelper: 'Alejandro Rodriguez',
+            nameError: 'Nombre (campo obligatorio)',
+            phoneError: 'Número incorrectamente especificado',
+            countryError: 'Debe introducir un país',
+            isDefault: true
+        }
+    };
+})();
---
+++ assets/js/translater.js
@@ -0,0 +1,179 @@
+var new_lang = {
+
+"lt0": "El tratamiento de <span class=\"order-title_green\">prostatitis</span> ",
+
+"lt1": "con remedio natural",
+
+"lt2": "Sin<br>dolor",
+
+"lt3": "Sin<br>operaciones",
+
+"lt4": "Sin masaje<br>rectal",
+
+"lt5": "Pedir ahora<br> <span class=\"form-title_green\">con descuento</span> ",
+
+"lt8": "País<select class=\"form-input\"><option>España</option></select>",
+
+"lt9": "Tu nombre: <input class=\"form-input\" type=\"text\">",
+
+"lt10": "Número de teléfono: <input class=\"form-input\" type=\"text\">",
+
+"lt11": "La oferta acaba en:",
+
+"lt12": "pedir",
+
+"lt13": "Enfermedad \"incómoda\" y <span class=\"text-red\">un montón de consecuencias</span> ",
+
+"lt14": "La prostatitis es una de las enfermedades que requieren mayor atención. Si no comienzas el tratamiento a tiempo, existe el riesgo de complicaciones graves ",
+
+"lt15": "Las más comunes son:",
+
+"lt16": "Impotencia",
+
+"lt17": "Infertilidad",
+
+"lt18": "Adenoma de próstata",
+
+"lt19": "Incontinencia urinaria",
+
+"lt20": "Dolor pélvico crónico",
+
+"lt21": "Cáncer de próstata",
+
+"lt22": "Tratamiento conservador de <span class=\"text-red\">prostatitis</span> ",
+
+"lt23": "Masaje rectal",
+
+"lt24": "Procedimiento de masaje prostático manual. La mayoría de los hombres encuentran este tratamiento doloroso y humillante.",
+
+"lt25": "Puede ayudar en las etapas iniciales de la enfermedad.",
+
+"lt26": "Costo: <span class=\"text-semiBold\">desde 200 euros.</span> ",
+
+"lt27": "Medicamentos",
+
+"lt28": "El tratamiento con medicamentos requiere una selección cuidadosa de los mismos. Muchos tienen efectos secundarios extremadamente desagradables.",
+
+"lt29": "Te puede ayudar si puedes encontrar el medicamento adecuado.",
+
+"lt30": "Costo: <span class=\"text-semiBold\">desde 150 euros.</span> ",
+
+"lt31": "Operación",
+
+"lt32": "No da una garantía del cien por cien de recuperación. Casos frecuentes de complicaciones postoperatorias y recaídas (retorno de los síntomas de prostatitis).",
+
+"lt33": "Solo puede ayudar en 25% de casos.",
+
+"lt30": "Costo: <span class=\"text-semiBold\">desde 1000 euros.</span> ",
+
+"lt35": "Métodos tradicionales",
+
+"lt36": "Existe un gran riesgo de dañar tu cuerpo y agravar la condición de la glándula prostática. El efecto de los métodos tradicionales de tratamiento no ha sido probado clínicamente.",
+
+"lt37": "Pueden ayudar como prevención de la enfermedad.",
+
+"lt30": "Costo: <span class=\"text-semiBold\">desde 10 euros.</span> ",
+
+"lt39": "XX – <span class=\"text-green\">ayudará a todos</span> ",
+
+"lt40": "Restaura completamente la salud masculina y alivia los síntomas de la prostatitis:",
+
+"lt41": "alivia el dolor al orinar",
+
+"lt42": "reduce la necesidad de ir al baño, incluso por la noche",
+
+"lt43": "aumenta la actividad sexual",
+
+"lt44": "reduce el riesgo de tumores",
+
+"lt45": "previene la recurrencia de la enfermedad",
+
+"lt46": "previene la recurrencia de la enfermedad",
+
+"lt47": " <span class=\"text-green\">El poder de la naturaleza</span> en cada componente",
+
+"lt48": "Ciruelo africano",
+
+"lt49": "Reduce la hinchazón y la inflamación de la próstata, retrasa el desarrollo de inflamación aséptica, previene el crecimiento excesivo de fibroblastos en la próstata, previene la formación de tejido tumoral.",
+
+"lt50": "Licopeno",
+
+"lt51": "Combate todos los factores del envejecimiento sexual masculino. Es un antioxidante, previene el crecimiento y la migración del tejido tumoral, reduce el riesgo de cáncer de próstata.",
+
+"lt52": "Sabal palmetto",
+
+"lt53": "Estimula la función del sistema hormonal en la producción de testosterona. Fortalece la potencia. Previene la formación de tejido fibroso.",
+
+"lt54": "Semillas de calabaza",
+
+"lt55": "Restauran el mecanismo de erección y fortalecen la inmunidad. Previenen el desarrollo de la inflamación de la próstata, elimina la congestión en el sistema urinario.",
+
+"lt56": "Opinión del especialista",
+
+"lt57": "La prostatitis le da al hombre muchos inconvenientes: sensación de ardor al orinar, dolor agudo en el área púbica, necesidad frecuente de usar el baño. La falta de tratamiento para esta enfermedad conduce al cáncer de próstata, impotencia e infertilidad.",
+
+"lt58": "En la práctica se puede curar la prostatitis de tres formas: 1) cirugía con rehabilitación a largo plazo y posibles recaídas; 2) tomando antibióticos, que tienen muchos efectos secundarios y no siempre funcionan; 3) un remedio natural recientemente desarrollado XX.",
+
+"lt59": "Primero recomiendo XX a todos los pacientes. No tiene efectos secundarios y combate la enfermedad incluso en su forma más grave. Este remedio proporciona al cuerpo del hombre todas las vitaminas y minerales necesarios para combatir la inflamación de la glándula prostática.",
+
+"lt60": "Antonio María Marcos, urólogo",
+
+"lt61": "Experiencia laboral: 23 años",
+
+"lt62": "Comentarios sobre XX",
+
+"lt69": "Arturo, <span class=\"age\">41 años</span> ",
+
+"lt70": "Durante más de un año me molestaba el dolor agudo en la parte inferior del abdomen, luego comencé a tener fiebre y ardor en el perineo. Los antibióticos recetados por el médico dieron un efecto a corto plazo. Lo mismo con los masajes. Tan pronto como comencé a tomar XX, noté signos de recuperación: dejé de ir frecuentemente al baño, los dolores constantes desaparecieron y la potencia mejoró. Después del curso, de la prostatitis solo me quedan recuerdos.",
+
+"lt71": "Miguel, <span class=\"age\">55 años</span> ",
+
+"lt72": "La prostatitis no es solo una enfermedad, sino toda una serie de limitaciones y sufrimientos. En primer lugar, la necesidad frecuente de ir al baño no te permite alejarte mucho de tu casa. El dolor al orinar es incluso difícil de describir. Al principio intenté tratarme en casa; no funcionó, solo empeoró. Aprendí sobre XX en el foro. Comencé a tomarlo e inmediatamente comencé a recuperarme. Solo un curso fue suficiente para deshacerme de la prostatitis! Sin cirugías ni masajes.",
+
+"lt73": "Mario, <span class=\"age\">32 años</span> ",
+
+"lt74": "Me enfrenté a la prostatitis a los 30 años. Me sorprendió mucho, pensé que esta historia solo concierne a personas mayores. Una vez le conté a un amigo sobre mi problema. Resultó que también se enfrentó a tal problema, se deshizo con éxito de él y me aconsejó que tomara XX. Después de dos semanas de tomarlo, me sentí genial de nuevo, como si nada hubiera pasado. En un curso, la prostatitis desapareció. Recomiendo este remedio.",
+
+"lt75": "Carmen, <span class=\"age\">46 años</span> ",
+
+"lt76": "Mi marido sufría de prostatitis durante mucho tiempo. Ya no podía ver su sufrimiento y comencé a estudiar este tema en Internet. Una vez me encontré con comentarios sobre el medicamento XX. La gente escribe sobre su recuperación con su ayuda, incluso aquellos que han tenido casos muy avanzados. Me inspiré, convencí a mi pareja de probar este remedio, aunque él ya no creía en absoluto en el resultado positivo. ¡Pues ahora mi marido está totalmente sano!:)",
+
+"lt85": "¡OJO!<br>¡TENGAN EN CUENTA!",
+
+"lt86": "El número de paquetes de XX necesarios para el resultado deseado <span class=\"alarm-bold\">se determina individualmente y puede aumentar en casos individuales.</span> ",
+
+"lt87": "El tratamiento de <span class=\"order-title_green\">prostatitis</span> ",
+
+"lt88": "con remedio natural",
+
+"lt89": "Sin<br>dolor",
+
+"lt90": "Sin<br>operaciones",
+
+"lt91": "Sin masaje<br>rectal",
+
+"lt92": "Pedir ahora<br> <span class=\"form-title_green\">con descuento</span> ",
+
+"lt95": "País<select class=\"form-input\"><option>España</option></select>",
+
+"lt96": "Tu nombre: <input class=\"form-input\" type=\"text\">",
+
+"lt97": "Número de teléfono: <input class=\"form-input\" type=\"text\">",
+
+"lt98": "La oferta acaba en:",
+
+"lt99": "pedir",
+
+
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
